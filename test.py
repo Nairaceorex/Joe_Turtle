68a50bca23d238b59b3e295cfbdd7b11b1a82f10
Nairaceorex <wwwolf2002@gmail.com>
2021-12-03 13:37:00 +0300

    blet

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from main import fib
 from unittest import TestCase
+
+
 class FibonacciTestCase(TestCase):
+
     def test_first_fibonacci_number_is_1(self):
         x = fib(1)
         self.assertEqual(5, x)
