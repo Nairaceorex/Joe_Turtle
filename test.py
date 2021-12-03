fb723fa29c1254622ce027dfe39a42e674d99c54
Nairaceorex <wwwolf2002@gmail.com>
2021-12-03 10:06:55 +0300

    test fib + fin func

---
+++ test.py
@@ -0,0 +1,6 @@
+from main import fib
+from unittest import TestCase
+class FibonacciTestCase(TestCase):
+    def test_first_fibonacci_number_is_1(self):
+        x = fib(1)
+        self.assertEqual(1, x)
