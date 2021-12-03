247f300a69f8bd1ec6866be83baa408bc8432eb8
Nairaceorex <wwwolf2002@gmail.com>
2021-12-03 13:09:38 +0300

    pepe

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+
+if __name__ == '__main__':
+    if teamcity.is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+
+    unittest.main(testRunner=runner)
+
 def fib(n):
     if n>1:
         return fib(n-1)+fib(n-2)
--- test.py
+++ test.py
@@ -3,12 +3,6 @@ from unittest import TestCase
 class FibonacciTestCase(TestCase):
     def test_first_fibonacci_number_is_1(self):
         x = fib(1)
-        self.assertEqual(1, x)
+        self.assertEqual(5, x)
 
-    def test_second_fibonacci_number_is_1(self):
-        x = fib(2)
-        self.assertEqual(1, x)
-
-    def test_5th_fibonacci_number_is_5(self):
-        x = fib(5)
-        self.assertEqual(7, x)
+    
