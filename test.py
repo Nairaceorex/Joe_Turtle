439bf732cf89708c43e2d66211af13719964a66e
Nairaceorex <wwwolf2002@gmail.com>
2021-12-03 10:14:09 +0300

    more tests

--- test.py
+++ test.py
@@ -4,3 +4,11 @@ class FibonacciTestCase(TestCase):
     def test_first_fibonacci_number_is_1(self):
         x = fib(1)
         self.assertEqual(1, x)
+
+    def test_second_fibonacci_number_is_1(self):
+        x = find_fibonacci(2)
+        self.assertEqual(1, x)
+
+    def test_5th_fibonacci_number_is_5(self):
+        x = find_fibonacci(5)
+        self.assertEqual(5, x)
